5f4fb81c5de970c2ab213cede4778fbea480e845
samuelhg200 <64262060+samuelhg200@users.noreply.github.com>
2022-11-28 00:26:20 +0000

    Initial Commit

---
+++ swimpal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swimpal
+//
+//  Created by Samuel Hervás Gómez on 11/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swimpal/swimpalApp.swift
@@ -0,0 +1,17 @@
+//
+//  swimpalApp.swift
+//  swimpal
+//
+//  Created by Samuel Hervás Gómez on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct swimpalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
